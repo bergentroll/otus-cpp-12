772ccfd31162dcc0d182096988b1da44cd11a72a
Anton Karmanov <bergertroll@insiberia.net>
2020-06-06 19:01:39 +0500

    Add arg parsing

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
+
+using namespace std;
+
+class InvalidArgument: public std::logic_error
+{
+public:
+  explicit InvalidArgument(const std::string &input):
+  std::logic_error(input) { }
+};
+
+
+int get_arg(int argc, char *argv[]) {
+  int N;
+  size_t idx;
+
+  if (argc != 2) throw(InvalidArgument("one agument expected"));
+
+  try {
+    N = stoi(argv[1], &idx);
+  } catch (invalid_argument &e) {
+    throw InvalidArgument("argument must be integer");
+  } catch (out_of_range &e) {
+    throw InvalidArgument("argument is out of range");
+  }
+
+  if (idx < strlen(argv[1])) {
+    throw InvalidArgument("argument must be integer");
+  }
+  if (N < 1) {
+    throw InvalidArgument("argument must be greater than zero");
+  }
+
+  return N;
+}
+
+int main(int argc, char *argv[]) {
+  int N;
+  try {
+    N = get_arg(argc, argv);
+  } catch (InvalidArgument &e) {
+    cerr << "Usage error: " << e.what() << endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
