42b82b23ef21aa28efacd8686c0cfc346d7659e1
Anton Karmanov <bergertroll@insiberia.net>
2020-06-07 21:08:17 +0500

    Rename Handler::p -> Handler::parser

--- inc/parser.hpp
+++ inc/parser.hpp
@@ -37,64 +37,64 @@ private:
 
   class Plain: public Handler {
   public:
-    Plain(Parser &p): p(p) { }
+    Plain(Parser &parser): parser(parser) { }
 
     HandlerPtr readToken(std::string const & token) override {
       if (token == "{") {
-          if (p.commands.size() > 0) p.flushCommands(); // TODO Notify.
-          return HandlerPtr(new Block(p));
+          if (parser.commands.size() > 0) parser.flushCommands();
+          return HandlerPtr(new Block(parser));
       }
       else if (token == "}") {
         throw InvalidToken(token);
       } else {
-        p.commands.push_back(token); // TODO Notify.
-        if (p.commands.size() >= p.packSize) p.flushCommands();
+        parser.commands.push_back(token);
+        if (parser.commands.size() >= parser.packSize) parser.flushCommands();
       }
-      return HandlerPtr(new Plain(p));
+      return HandlerPtr(new Plain(parser));
     }
 
   private:
-    Parser &p;
+    Parser &parser;
   };
 
   class Block: public Handler {
   public:
-    Block(Parser &p): p(p) { }
+    Block(Parser &parser): parser(parser) { }
 
     HandlerPtr readToken(std::string const & token) override {
       if (token == "{") {
-        return HandlerPtr(new Nested(p, 1));
+        return HandlerPtr(new Nested(parser, 1));
       } else if (token == "}") {
-        if (p.commands.size() > 0) p.flushCommands();
-        return HandlerPtr(new Plain(p));
+        if (parser.commands.size() > 0) parser.flushCommands();
+        return HandlerPtr(new Plain(parser));
     } else {
-        p.commands.push_back(token);
+        parser.commands.push_back(token);
     }
-    return HandlerPtr(new Block(p));
+    return HandlerPtr(new Block(parser));
   }
 
   private:
-    Parser &p;
+    Parser &parser;
   };
 
   class Nested: public Handler {
   public:
-    Nested(Parser &p, int level): p(p), level(level) { }
+    Nested(Parser &parser, int level): parser(parser), level(level) { }
 
     HandlerPtr readToken(std::string const & token) override {
       if (token == "{") {
         ++level;
       } else if (token == "}") {
         --level;
-        if (level == 0) return HandlerPtr(new Block(p));
+        if (level == 0) return HandlerPtr(new Block(parser));
       } else {
-        p.commands.push_back(token);
+        parser.commands.push_back(token);
       }
-      return HandlerPtr(new Nested(p, level));
+      return HandlerPtr(new Nested(parser, level));
     }
 
   private:
-    Parser &p;
+    Parser &parser;
     int level;
   };
 
