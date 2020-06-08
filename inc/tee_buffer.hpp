61c1ddbceeae97e879998e62475916003b8042d4
Anton Karmanov <bergertroll@insiberia.net>
2020-06-08 22:44:48 +0500

    Make TeeBuffer write to file

--- inc/tee_buffer.hpp
+++ inc/tee_buffer.hpp
@@ -1,16 +1,32 @@
 #ifndef TEE_BUFFER_HPP
 #define TEE_BUFFER_HPP
 
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 
 class TeeBuffer: public std::stringbuf{
 public:
+  ~TeeBuffer() override {
+    sync();
+    file.close();
+  }
+
+  TeeBuffer() {
+    std::filesystem::path const path = "file.txt";
+    file = std::ofstream(path);
+  }
+
   int sync() override {
-    std::cout << '!' << str().c_str();
+    std::cout << str();
+    file << str();
     str("");
     return 0;
   }
+
+private:
+  std::ofstream file;
 };
 
 #endif
